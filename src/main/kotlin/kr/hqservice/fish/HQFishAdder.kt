16139c64d8d9eef0b541e2a4b0b8cf6d7e18f0b1
CosineAA <rhdwlgns0725@gmail.com>
2023-04-27 11:04:13 +0900

    Initial commit

---
+++ src/main/kotlin/kr/hqservice/fish/HQFishAdder.kt
@@ -0,0 +1,18 @@
+package kr.hqservice.fish
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HQFishAdder : JavaPlugin() {
+
+    override fun onLoad() {
+
+    }
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
